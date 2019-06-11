451379223684fea682f4dee7ea1b9b518374b992
br71 <branko.sretenovic@gmail.com>
2019-06-11 11:29:52 +0200

    Commit 11.06.2019-2, 009.MergeLists.py - removed lambda

--- 009.MergeLists.py
+++ 009.MergeLists.py
@@ -3,9 +3,14 @@ from functools import reduce
 # Merge two list node by node, ascent order of nodes.
 def mergeTwoLists(l1, l2):
 
-    a = list(zip(l1,l2))
+    t = []
 
-    print(sorted(reduce(lambda x, y: x + list(y),a,[])))
+    #The method extend() appends the contents of argument to list
+    # Zip function will pair nodes of list
+    for i in list(zip(l1,l2)):
+        t.extend(list(i))
+
+    print(sorted(t))
 
 l1 = [1,2,4]
 l2 = [5,3,1]
